6b7647b18a96879adef8345efcbf3a3b195ebf90
demost <demost@5c3a0519-178f-12a8-b0a2-772f4f2558d5>
2012-03-12 20:06:48 +0000

    Adicionado a primeira versão do matcher.

---
+++ src-br.pucrio.inf.les.genarch/br/pucrio/inf/les/matcher/util/GenerativeModelDiffEngine.java
@@ -0,0 +1,7 @@
+package br.pucrio.inf.les.matcher.util;
+
+import org.eclipse.emf.compare.diff.engine.GenericDiffEngine;
+
+public class GenerativeModelDiffEngine extends GenericDiffEngine {
+	//TODO [Demóstenes]
+}
